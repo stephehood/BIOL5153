84e3b5d49f3773768f36b2bbb3fdee4f3c29afef
seg013 <seg013@uark.edu>
2018-04-09 16:49:07 -0500

    updates thanks to Andy and Haley

--- parseGFF.py
+++ parseGFF.py
@@ -1,46 +1,35 @@
+
 #!/usr/bin/env python3
 
 from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from collections import  defaultdict
+import csv
+# read in genome in FASTA format
 
-# read names and positions from bed file
-positions = defaultdict(list)
-with open ("watermelon.gff") as f:
-	for line in f:
-		name, start, stop = line.split()
-		positions[name].append(((start), (stop)))
+genome = SeqIO.read("watermelon.fsa", "fasta")
+print('sequence is ',len(genome.seq))
 
-# parse fasta file and turn into dictionary
+# open GFF file
+# read it in line by line using a for loop
 
-records = SeqIO.to_dict(SeqIO.parse(open("watermelon.fasta", "fasta"))
-# search for seqences
+gff = open("watermelon.gff", 'r')
+for line in gff:
+	words = line.split("\t")
+	species = (words[0])
+	start = int(words[3])
+	end = int(words[4])
+	gene = (words[8])
+	print(">" + species + " | " + gene + genome.seq[start-1:end]) 
+# use split command to split each line into a lists
+gff.close() 
 
-for name in positions:
-	for (start, stop) in postions[name]:
-		long_seq_record = records[name]
-		long_seq = long_seq_record.seq
-		alphabet = long_seq.alphabet
-		sequence_record =  SeqRecord(Seq(long_seq, alphabet) id.name, description='')
-		sequence_record.append(sequence_record)
-# write to file
-with open ('homework_5.fasta', 'w') as f:
-	SeqIO.write(sequence_record, f, 'fasta')
-#for record in SeqIO.parse ("watermelon.fsa", "fasta"):
-	#print(record.id)
-	#print('sequence is ', record.seq)
+"""
+with open("watermelon.gff") as tsv:
+	for line in csv.reader(tsv, delimiter="\t"):
+		print zip(*[line.split() for line in tsv])[4:6]
+"""
+#use beginning and end coordinates to extract the sequence from the genome
 
-#seq_size = open ("watermelon.gff")
-#seq_data = seq_size.read()
-#print (seq_data)
-# need to turn the text into a list of list
-
-#import csv
-
-#with open("watermelon.gff") as tsv:
-	#for line in csv.reader(tsv, delimiter="\t"):
-		#print zip(*[line.split() for line in tsv])[4:6]
+"""
 
 # goal of this is to extract the sequences from watermelon.fasta (done)
 # use the watermelon.gff to obtain the sequence locations/ lengths (ok)
@@ -57,3 +46,38 @@ with open ('homework_5.fasta', 'w') as f:
 # have genome, beginning and end coordinate
 # print to screen
 # close GFF file
+
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from collections import  defaultdict
+
+# read names and positions from bed file
+positions = defaultdict(list)
+with open ("watermelon.gff") as f:
+        for line in f:
+                name, start, stop = line.split()
+                positions[name].append(((start), (stop)))
+seq_size = open ("watermelon.gff")
+seq_data = seq_size.read()
+print (seq_data)
+
+
+# parse fasta file and turn into dictionary
+
+records = SeqIO.to_dict(SeqIO.parse(open("watermelon.fsa", "fasta"))
+# search for seqences
+
+for record in SeqIO.parse("watermelon.fsa", "fasta"):
+        print(record.id)
+        print (sequence is' , record.seq)
+        #for (start, stop) in postions[name]
+                #long_seq_record = records[name]
+                #long_seq = long_seq_record.seq
+                #alphabet = long_seq.alphabet
+                #sequence_record =  SeqRecord(Seq(long_seq, alphabet) id.name, description='')
+                #sequence_record.append(sequence_record)
+# write to file
+with open ('homework_5.fasta', 'w') as f:
+        SeqIO.write(sequence_record, f, 'fasta')
+"""
+
